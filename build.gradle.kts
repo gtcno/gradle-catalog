802d527d24652d4286cc37fcc91b946b10b58242
Giao The Cung <giaothe@gmail.com>
2021-11-30 11:44:26 +0100

    Hubba

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,11 +10,17 @@ catalog {
     }
 }
 
+val tomlFile = layout.buildDirectory.file("/libs.versions.toml")
+val tomlArtifact = artifacts.add("archives", tomlFile.get().asFile) {
+    type = "toml"
+}
+
 publishing {
     publications {
         create<MavenPublication>("maven") {
             this.groupId = "no.gtc.common"
             this.artifactId = "versionCatalog"
+            this.artifact(tomlArtifact)
             from(components["versionCatalog"])
         }
     }
