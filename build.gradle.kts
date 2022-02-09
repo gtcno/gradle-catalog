a16aec6937bb35f8e8ebd97069c1102f3035dbe6
Giao The Cung <giaothe@gmail.com>
2022-02-09 09:17:38 +0100

    Build

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,11 +15,16 @@ publishing {
     repositories {
         maven {
             url = uri("https://maven.pkg.github.com/gtcno/gradle-catalog")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
         }
     }
     publications {
         create<MavenPublication>("maven") {
             this.groupId = "com.github.gtcno"
+            this.version = findProperty("RELEASE_TAG").toString()
             from(components["versionCatalog"])
         }
     }
