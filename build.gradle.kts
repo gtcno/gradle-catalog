c13fc2a05f686e616bd4936088b9a01d318173a7
Giao The Cung <giaothe@gmail.com>
2022-02-23 10:19:10 +0100

    Clean

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,23 @@
 plugins {
-    base
-    `version-catalog`
-    `maven-publish`
-}
-
-catalog {
-    versionCatalog {
-        from(files("$projectDir/libs.versions.toml"))
-    }
 }
 
 
-publishing {
-    repositories {
-        maven {
-            url = uri("https://maven.pkg.github.com/gtcno/gradle-catalog")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
-        }
-    }
-    publications {
-        create<MavenPublication>("maven") {
-            this.groupId = "com.github.gtcno"
-            this.version = findProperty("RELEASE_TAG").toString()
-            from(components["versionCatalog"])
-        }
-    }
-}
+//publishing { repositories {
+//        maven {
+//            url = uri("https://maven.pkg.github.com/gtcno/gradle-catalog")
+//            credentials {
+//                username = System.getenv("GITHUB_ACTOR")
+//                password = System.getenv("GITHUB_TOKEN")
+//            }
+//        }
+//    }
+//    publications {
+//        create<MavenPublication>("maven") {
+//            this.groupId = "com.github.gtcno"
+//            this.version = findProperty("RELEASE_TAG").toString()
+//            from(components["versionCatalog"])
+//        }
+//    }
+//}
 
 
