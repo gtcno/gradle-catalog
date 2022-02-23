0ce6556cf40b2c8edfa4fbe8fcf4092a5e3ee26b
Giao The Cung <giaothe@gmail.com>
2022-02-23 11:15:03 +0100

    HUbba

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,26 @@
 plugins {
+    `maven-publish`
+    `java-gradle-plugin`
+    `kotlin-dsl`
+}
+
+gradlePlugin{
+    plugins{
+        create("kotlin-app") {
+            id = "no.nav.dagpenger"
+            implementationClass = "KotlinAppConventionsPlugin"
+        }
+    }
+}
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+}
+
+repositories {
+    mavenCentral()
 }
 
 
-//publishing { repositories {
-//        maven {
-//            url = uri("https://maven.pkg.github.com/gtcno/gradle-catalog")
-//            credentials {
-//                username = System.getenv("GITHUB_ACTOR")
-//                password = System.getenv("GITHUB_TOKEN")
-//            }
-//        }
-//    }
-//    publications {
-//        create<MavenPublication>("maven") {
-//            this.groupId = "com.github.gtcno"
-//            this.version = findProperty("RELEASE_TAG").toString()
-//            from(components["versionCatalog"])
-//        }
-//    }
-//}
-
+publishing {
+}
 
