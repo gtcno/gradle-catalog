396b43982b9f8d6f3ea64e2e941cec7c9f5b7118
Giao The Cung <giaothe@gmail.com>
2022-11-04 08:48:31 +0100

    clean up

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,14 @@
 plugins {
     `maven-publish`
-    `java-gradle-plugin`
     `kotlin-dsl`
 }
 
 group = "no.gtc.gradle"
 version = "0.0.1"
 
-
-gradlePlugin {
-    plugins {
-        create("kotlin-app") {
-            id = "kotlin-app"
-            implementationClass = "KotlinAppConventionsPlugin"
-        }
-    }
-}
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
 }
 
-repositories {
-    mavenCentral()
-}
 
 
 publishing {
